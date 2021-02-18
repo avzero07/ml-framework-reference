9f6a27666890c7015c85530cbc6355fdd69bd0e6
avzero07 <akshay.viswakumar@gmail.com>
2021-02-18 14:33:58 -0800

    added test for read_idx.get_headers(). all tests passing

--- util/read_idx.py
+++ util/read_idx.py
@@ -3,19 +3,26 @@ Contains Functions to Read IDX Files
 '''
 
 '''
-Loops Through The File and Prints Out Headers
-
+Loops Through The File and Collects Header Values
 Assumes Every Header is 32-bits Long
+Returns List of Header Values in Order -> [H1,H2,H3,...]
 '''
-def print_headers(input_file_path,offset_data_starti,is_big_endian=True):
-    # Open File
+def get_headers(input_file_path,offset_data_start,is_big_endian=True):
     if is_big_endian:
         byteorder_string = 'big'
     else:
         byteorder_string = 'little'
 
+    header_list = list()
+
+    # Open File
     ip_file = open(input_file_path,'rb')
+
+    i = 0
     while(offset_data_start):
         val = int.from_bytes(ip_file.read(4), byteorder=byteorder_string)
-        print("H1 = {}".format(val))
-        offset_data_start-=1
+        header_list.append(val)
+        offset_data_start-=4
+        i+=1
+    
+    return header_list
--- util/test/test_read_idx.py
+++ util/test/test_read_idx.py
@@ -4,18 +4,46 @@ Tests for Functions in read_idx.py
 import sys
 sys.path.append('../')
 
+import pytest
 import read_idx as rd
 import tempfile
 import subprocess
 
-def test_load_header_lecun_mnist():
+@pytest.fixture(scope="module",autouse=True)
+def print_new_line():
+    print("")
+
+def test_get_header_lecun_mnist():
     with tempfile.TemporaryDirectory() as dirpath:
-        file_path = "../../data/mnist/train-images-idx3-ubyte.gz"
-        extracted_file_path = ''.join([dirpath,'/','op_file'])
+        file_name = "train-images-idx3-ubyte.gz"
+        file_path = "../../data/mnist/"
         
-        # Extract GZ to Temp Directory
-        op = subprocess.run(['zcat',file_path,'>',extracted_file_path],stderr=subprocess.PIPE,stdout=subprocess.PIPE,shell=True)
-        print(op.stderr.decode())
+        # Copy GZ to Temp Directory
+        op = subprocess.run(['cp',''.join([file_path,file_name]),''.join([dirpath,'/'])])
+        assert not op.returncode, "Unable to Copy {} to temp directory {}".format(file_name,dirpath)
+        
+        # Extract File in Temp Directory
+        op = subprocess.run(['gunzip',''.join([dirpath,'/',file_name])],stderr=subprocess.PIPE,stdout=subprocess.PIPE)
         assert not op.returncode, "Received errorcode {} when trying to uncompress {}".format(op.returncode,file_path)
 
-        subprocess.run(['ls','-ltr',dirpath])
+        # Run get_headers()
+        header_list = rd.get_headers(''.join([dirpath,'/',file_name[:-3]]),16)
+
+        '''
+        LeCun's IDX Format Has The Following Structure For 'train-images-idx3-ubyte'
+
+        [offset] [type]          [value]          [description]
+        0000     32 bit integer  0x00000803(2051) magic number
+        0004     32 bit integer  60000            number of images
+        0008     32 bit integer  28               number of rows
+        0012     32 bit integer  28               number of columns
+        0016     unsigned byte   ??               pixel
+        0017     unsigned byte   ??               pixel
+        ........
+        xxxx     unsigned byte   ??               pixel
+        '''
+
+        true_header_list = [2051,60000,28,28]
+
+        for i in range(len(header_list)):
+            assert true_header_list[i] == header_list[i], "Header{} Mismatch!".format(i)
